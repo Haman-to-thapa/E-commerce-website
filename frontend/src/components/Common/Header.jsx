72ef97f458f8773b91a4bfb92d4ad879205b2dfc
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-02-26 17:23:12 +0530

    add header file and also add Topbar file and all main file are under Userlayout file

---
+++ frontend/src/components/Common/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Topbar from '../Layout/Topbar'
+
+const Header = () => {
+  return (
+    <div>
+      {/* Topbar */}
+
+      <Topbar />
+
+      {/*  navbar */}
+      {/* Cart Drawer */}
+
+    </div>
+  )
+}
+
+export default Header
+Headers
