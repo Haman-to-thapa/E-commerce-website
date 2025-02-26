f3d4f7b8eb4157a7608e5850dad50ba47a3b60a7
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-02-26 19:03:21 +0530

    all push

--- frontend/src/components/Common/Header.jsx
+++ frontend/src/components/Common/Header.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import Topbar from '../Layout/Topbar'
+import Navbar from './Navbar'
 
 const Header = () => {
   return (
-    <div>
+    <header className='border-b border-gray-300'>
       {/* Topbar */}
 
       <Topbar />
 
       {/*  navbar */}
+
+      <Navbar />
       {/* Cart Drawer */}
 
-    </div>
+    </header>
   )
 }
 
