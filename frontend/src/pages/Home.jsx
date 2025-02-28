0bcda30c7c99e18db8a7bcd21dbd165041d924f9
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-02-28 19:19:28 +0530

    add route on Home element

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../components/Layout/Hero'
+import GenderCollectionSecion from '../components/Products/GenderCollectionSecion'
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <GenderCollectionSecion />
+    </div>
+  )
+}
+
+export default Home
