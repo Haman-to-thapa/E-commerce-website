1f1a0a349dadf807dee2a37258abd9941dbbbfe0
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-03-01 11:02:04 +0530

    NewArrivals in add on Home screen

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Layout/Hero'
 import GenderCollectionSecion from '../components/Products/GenderCollectionSecion'
+import NewArrivals from '../components/Products/NewArrivals'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <GenderCollectionSecion />
+      <NewArrivals />
     </div>
   )
 }
