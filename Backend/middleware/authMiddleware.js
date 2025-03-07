0801a6773ed23b1d9716329909ea40d1c9b7db94
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-03-07 17:58:47 +0530

    middleware of login register to

---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,32 @@
+
+
+
+
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+
+// Middleware to protect routes
+const protect = async (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const secretKey = process.env.JWT_SECRET.trim();
+
+      const decoded = jwt.verify(token, secretKey);
+
+      req.user = await User.findById(decoded.user._id).select("-password");
+      next();
+
+    } catch (error) {
+      console.error("Token verification failed", error);
+      return res.status(401).json({ message: "Not authorized, token failed" });
+    }
+  } else {
+    return res.status(401).json({ message: "Not authorized, no token provided" });
+  }
+};
+
+export default protect;
