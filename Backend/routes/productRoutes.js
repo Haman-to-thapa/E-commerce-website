10a1198d9239bfe331a6000ee90321296b438fb9
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-03-08 17:09:23 +0530

    delete product function will be execuate in router

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -1,14 +1,14 @@
-import express from 'express';
+import express from "express";
 import protect from "../middleware/authMiddleware.js";
-import Product from '../models/product.js';
-import  admin  from '../middleware/adminMiddleware.js'
+import Product from "../models/product.js";
+import admin from "../middleware/adminMiddleware.js";
 
 const router = express.Router();
 
 // @route POST /api/products
 // @desc Create a new Product
 // @access Private/Admin
-router.post('/products', protect, admin, async (req, res) => {
+router.post("/products", protect, admin, async (req, res) => {
   try {
     const {
       name,
@@ -21,7 +21,7 @@ router.post('/products', protect, admin, async (req, res) => {
       brand,
       sizes,
       colors,
-      collection, 
+      collections,
       material,
       gender,
       images,
@@ -29,7 +29,7 @@ router.post('/products', protect, admin, async (req, res) => {
       isPublished,
       tags,
       dimensions,
-      weight
+      weight,
     } = req.body;
 
     // Correct model name
@@ -44,7 +44,7 @@ router.post('/products', protect, admin, async (req, res) => {
       brand,
       sizes,
       colors,
-      collection,  
+      collections,
       material,
       gender,
       images,
@@ -64,5 +64,92 @@ router.post('/products', protect, admin, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route PUT / api/products/:id
+// @desc Update an existing product ID
+// @access Private/Admi
+router.put("/:id", protect, admin, async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      disCountPrice,
+      countInStock,
+      sku,
+      category,
+      brand,
+      sizes,
+      colors,
+      collections,
+      material,
+      gender,
+      images,
+      isFeatured,
+      isPublished,
+      tags,
+      dimensions,
+      weight,
+    } = req.body;
+
+    // Find product by ID
+    const product = await Product.findById(req.params.id);
+    
+    if(product) {
+      //update product fields
+      product.name = name || product.name;
+      product.description = description || product.description;
+      product.price = price || product.price;
+      product.disCountPrice = disCountPrice || product.disCountPrice;
+      product.countInStock = countInStock || product.countInStock;
+      product.sku = sku || product.sku;
+      product.category = category || product.category;
+      product.brand = brand || product.brand;
+      product.sizes = sizes || product.sizes;
+      product.colors = colors || product.colors;
+      product.collections = collections || product.collections;
+      product.material = material || product.material;
+      product.gender = gender || product.gender;
+      product.images = images || product.images;
+      product.isFeatured = isFeatured !== undefined ? isFeatured : product.isFeatured;
+
+      product.isPublished = isPublished !== undefined ? isPublished : product.isPublished;
+      product.tags = tags || product.tags;
+      product.dimensions = dimensions || product.dimensions;
+      product.weight = weight || product.weight;
+      
+      // save the update product 
+      const updateProduct = await product.save();
+      res.json(updateProduct)
+    } else {
+      res.status(404).json({message :" product not found"})
+    }
+  } catch (error) {
+    console.error(error)
+    res.status(401).json({message: "Server Error"})
+  }
+});
+
+// @route DELELTE /api/products/:id
+//@desc Delete a proudct by ID
+// @access Private/Admin 
+
+router.delete('/:id', protect, admin, async(req, res) => {
+  
+  const product = await Product.findById(req.params.id);
+
+  try {
+    if(product) {
+      await product.deleteOne();
+      res.json({message : "Product Removed"});
+    } else {
+      res.status(404).json({message: "Product Not Found"})
+    }
+  } catch (error) {
+    console.error(error)
+    res.status(404).json({message:"Server Error"})
+  }
+
+})
 
 export default router;
