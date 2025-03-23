32023bd1a20da9787c8be1c17ae27bc0d605ee85
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-03-23 13:44:14 +0530

    navigate handleCheckout functon add redux routest add selectore dispatch

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import { IoMdClose } from 'react-icons/io'
 import CartContents from '../Cart/CartContents'
 import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const CartDrawer = ({ toggleCartDrawer, drawerOpen }) => {
+
+  const { user, guestId } = useSelector((state) => state.auth);
+  const { cart } = useSelector((state) => state.cart);
+  const userId = user ? user._id : null;
+
+
 
   const navigate = useNavigate();
   const handleCheckout = () => {
     toggleCartDrawer()
-    navigate('/checkout')
+    if (!user) {
+      navigate('/login?redireact=checkout')
+    } else {
+      navigate('/checkout');
+    }
   }
 
   return (
@@ -22,17 +33,29 @@ const CartDrawer = ({ toggleCartDrawer, drawerOpen }) => {
       {/* Cart contents with scrollale area  */}
       <div className='flex-grow p-4 overflow-y-auto'>
         <h2 className='text-xl font-semibold mb-4'>Your Cart</h2>
-        <CartContents />
+        {
+          cart && cart?.products?.length > 0 ? (
+            <CartContents cart={cart} userId={userId} guestId={guestId} />
+          ) : (<p> Your cart is empty</p>)
+        }
+
       </div>
 
       {/* Checkout button fixed at the bottom */}
       <div className='p-4 bg-white sticky bottom-0'>
-        <button
-          onClick={handleCheckout}
-          className='w-full text-white bg-black py-3 rounded-lg font-semibold hover:bg-gray-800 transition-all'>Checkout</button>
-        <p className='text-sm tracking-tighter text-gray-500 mt-2 text-center'>
-          Shipping , taxes , and discoutn codes calculated at checkout.
-        </p>
+        {
+          cart && cart?.products?.length > 0 && (
+            <>
+              <button
+                onClick={handleCheckout}
+                className='w-full text-white bg-black py-3 rounded-lg font-semibold hover:bg-gray-800 transition-all'>Checkout</button>
+              <p className='text-sm tracking-tighter text-gray-500 mt-2 text-center'>
+                Shipping , taxes , and discoutn codes calculated at checkout.
+              </p>
+            </>
+          )
+        }
+
       </div>
 
     </div>
