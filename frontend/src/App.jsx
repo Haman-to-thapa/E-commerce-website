6b4bd79203ed388dd1f996a2ae487759c81a507f
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-03-24 09:10:37 +0530

    cover adminLayout by protectRoutes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,6 +20,7 @@ import EditProductPage from './components/Admin/EditProductPage'
 import OrderManagement from './components/Admin/OrderManagement'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import ProtectedRoute from './components/Common/ProtectedRoute'
 
 
 const App = () => {
@@ -54,7 +55,9 @@ const App = () => {
 
 
           {/* Admin Layout */}
-          <Route path='/admin' element={<AdminLayout />}>
+          <Route path='/admin' element={<ProtectedRoute role="admin">
+            <AdminLayout />
+          </ProtectedRoute>}>
             <Route index element={<AdminHomePage />} />
             <Route path='users' element={<UserManagement />} />
             <Route path='products' element={<ProductManagement />} />
