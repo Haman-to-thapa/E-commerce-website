e1af7cb315ec8355460004dced91b7dcfd7d4afe
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-03-24 07:55:21 +0530

    id param dispatch orderdetails loading useEffect to connected to redux order

--- frontend/src/pages/OrderDetailsPage.jsx
+++ frontend/src/pages/OrderDetailsPage.jsx
@@ -1,39 +1,51 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom'
+import { fetchOrderDetails } from '../redux/slice/orderSlice';
 
 const OrderDetailsPage = () => {
 
   const { id } = useParams();
-  const [orderDetails, setOrderDetails] = useState(null);
+  const dispatch = useDispatch();
+  const { orderDetails, loading, error } = useSelector((state) => state.orders);
 
   useEffect(() => {
-    const mockOrderDetails = {
-      _id: id,
-      createdAt: new Date(),
-      isPaid: true,
-      isDelivered: false,
-      paymentMethod: "PayPal",
-      shippingMthod: "Standard",
-      shippingAddress: { city: "New York", country: "USA" },
-      orderItems: [
-        {
-          productId: "1",
-          name: "Jacket",
-          price: 120,
-          quantity: 1,
-          image: "https://picsum.photos/150?random=1",
-        },
-        {
-          productId: "2",
-          name: "Shirt",
-          price: 100,
-          quantity: 2,
-          image: "https://picsum.photos/150?random=2",
-        },
-      ],
-    };
-    setOrderDetails(mockOrderDetails)
-  }, [id]);
+    dispatch(fetchOrderDetails(id))
+  }, [dispatch, id])
+
+  if (loading) return <p>Loading....</p>
+  if (error) return <p>Error : {error}</p>
+
+  // const [orderDetails, setOrderDetails] = useState(null);
+
+  // useEffect(() => {
+  //   const mockOrderDetails = {
+  //     _id: id,
+  //     createdAt: new Date(),
+  //     isPaid: true,
+  //     isDelivered: false,
+  //     paymentMethod: "PayPal",
+  //     shippingMthod: "Standard",
+  //     shippingAddress: { city: "New York", country: "USA" },
+  //     orderItems: [
+  //       {
+  //         productId: "1",
+  //         name: "Jacket",
+  //         price: 120,
+  //         quantity: 1,
+  //         image: "https://picsum.photos/150?random=1",
+  //       },
+  //       {
+  //         productId: "2",
+  //         name: "Shirt",
+  //         price: 100,
+  //         quantity: 2,
+  //         image: "https://picsum.photos/150?random=2",
+  //       },
+  //     ],
+  //   };
+  //   setOrderDetails(mockOrderDetails)
+  // }, [id]);
 
   return (
     <div className='max-w-7xl mx-auto p-4 sm:p-6'>
@@ -89,12 +101,11 @@ const OrderDetailsPage = () => {
               </div>
             </div>
             {/* product list  */}
+            {/* Product List */}
             <div className="overflow-x-auto">
-              <h4 className='text-lg font-semibold mb-4'>
-                Products
-              </h4>
-              <table className='min-w-full text-gray-600 mb-4 '>
-                <thead className="bg-gray-100 ">
+              <h4 className="text-lg font-semibold mb-4">Products</h4>
+              <table className="min-w-full text-gray-600 mb-4">
+                <thead className="bg-gray-100">
                   <tr>
                     <th className="py-2 px-4">Name</th>
                     <th className="py-2 px-4">Unit Price</th>
@@ -103,28 +114,36 @@ const OrderDetailsPage = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  {orderDetails.orderItems.map((item) => (
-                    <tr key={item.productId} className='border-b'>
-                      <td className='py-2 px-4 flex items-center'>
-                        <img src={item.image} alt={item.name} className='w-12 h-12 object-cover rounded-lg mr-4' />
-
-                        <Link className='text-blue-500 hover:underline' to={`/product/${item.productId}`}>
-                          {item.name}
-                        </Link>
+                  {orderDetails?.orderItems.length > 0 ? (
+                    orderDetails.orderItems.map((item) => (
+                      <tr key={item?.productId} className="border-b">
+                        <td className="py-2 px-4 flex items-center">
+                          <img
+                            src={item?.image || "/placeholder.png"}
+                            alt={item?.name || "No Name"}
+                            className="w-12 h-12 object-cover rounded-lg mr-4"
+                          />
+                          <Link
+                            className="text-blue-500 hover:underline"
+                            to={`/product/${item?.productId}`}
+                          >
+                            {item?.name || "Unnamed Product"}
+                          </Link>
+                        </td>
+                        <td className="py-2 px-4">{item?.price || "0.00"}</td>
+                        <td className="py-2 px-4">{item?.quantity || 0}</td>
+                        <td className="py-2 px-4">
+                          $ {(item?.price * item?.quantity) || "0.00"}
+                        </td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan={4} className="py-4 px-4 text-center text-gray-500">
+                        No products found
                       </td>
-
-                      <td className='py-2 px-4 '>
-                        $ {item.price}
-                      </td>
-                      <td className='py-2 px-4 '>
-                        $ {item.quantity}
-                      </td>
-                      <td className='py-2 px-4 '>
-                        $ {item.price * item.quantity}
-                      </td>
-
                     </tr>
-                  ))}
+                  )}
                 </tbody>
               </table>
             </div>
