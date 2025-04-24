b2d3fd8d9640679f9b9bd6d24aa0706dd4baeaf3
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-04-24 20:26:54 +0530

    push

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,9 +8,15 @@ export default defineConfig({
   build: {
     outDir: 'build',
     assetsDir: 'assets',
+    emptyOutDir: true,
   },
   server: {
     port: process.env.PORT || 3000,
     host: '0.0.0.0', // This is important for Render
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
   },
 })
