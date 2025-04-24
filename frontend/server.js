27319fdc54f8dc094e297e32c089e6b39ed22aff
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-04-24 19:41:43 +0530

    fixed bug

--- frontend/server.js
+++ frontend/server.js
@@ -8,12 +8,12 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the dist directory
-app.use(express.static(path.join(__dirname, 'dist')));
+// Serve static files from the build directory
+app.use(express.static(path.join(__dirname, 'build')));
 
 // For any request that doesn't match a static file, serve index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
